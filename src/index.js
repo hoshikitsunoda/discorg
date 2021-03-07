5fb76096cecfccdd433df4d29ee854bd2e5bdf8c
hoshikitsunoda <hoshkitsunoda@gmail.com>
2021-03-07 01:03:29 -0800

    removed chakra

--- src/index.js
+++ src/index.js
@@ -2,23 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
-const colors = {
-  palette: {
-    black: '#0D0A0B',
-    grey: '#454955',
-    white: '#F3EFF5',
-    success: '#72B01D',
-    error: '#FF6542',
-  },
-}
-
-const theme = extendTheme({ colors })
-
-ReactDOM.render(
-  <ChakraProvider theme={theme}>
-    <App />
-  </ChakraProvider>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))

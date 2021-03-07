b410f6c897ce9f482f09e8e69ce42ea7f35d1089
hoshikitsunoda <hoshkitsunoda@gmail.com>
2021-03-07 01:15:34 -0800

    added material ui

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const theme = createMuiTheme({
+  palette: {
+    primary: '#F3EFF5',
+    secondary: '#0D0A0B',
+    grey: '#454955',
+    success: '#72B01D',
+    error: '#FF6542',
+  },
+})
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root')
+)

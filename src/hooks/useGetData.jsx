5869f64bc2fb66db980666513526db7e7f8ab8f5
hoshikitsunoda <hoshkitsunoda@gmail.com>
2021-03-09 14:58:23 -0800

    added useGetData hook

---
+++ src/hooks/useGetData.jsx
@@ -0,0 +1,27 @@
+import { useState, useCallback } from 'react'
+import { toast } from 'react-toastify'
+import { auth } from '../services/firebase'
+
+const useGetData = () => {
+  const [data, setData] = useState({})
+  const [loading, setLoading] = useState(false)
+
+  const getUser = useCallback(async () => {
+    setLoading(true)
+    try {
+      await auth().onAuthStateChanged((user) => {
+        if (user) {
+          setData(user)
+        }
+        setLoading(false)
+      })
+    } catch (err) {
+      toast.error(err.message)
+      setLoading(false)
+    }
+  }, [])
+
+  return { getUser, data, loading }
+}
+
+export default useGetData

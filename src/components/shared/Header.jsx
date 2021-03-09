d70638e747b1769e339d06393fcb81ad5a69c687
hoshikitsunoda <hoshkitsunoda@gmail.com>
2021-03-09 14:58:55 -0800

    shows email in header if logged in

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,5 +1,39 @@
+import { useEffect } from 'react'
+import { AppBar, Toolbar, Typography, IconButton } from '@material-ui/core'
+import AccountCircle from '@material-ui/icons/AccountCircle'
+
+import useGetData from '../../hooks/useGetData'
+
 const Header = () => {
-  return <div>Header</div>
+  const { data, getUser } = useGetData()
+
+  useEffect(() => {
+    getUser()
+  }, [getUser])
+
+  const { email, uid } = data
+
+  return (
+    <AppBar position="static" color="transparent">
+      <Toolbar>
+        {uid && (
+          <>
+            <div>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                color="primary"
+              >
+                <AccountCircle />
+              </IconButton>
+            </div>
+            <Typography variant="body1">{email}</Typography>
+          </>
+        )}
+      </Toolbar>
+    </AppBar>
+  )
 }
 
 export default Header

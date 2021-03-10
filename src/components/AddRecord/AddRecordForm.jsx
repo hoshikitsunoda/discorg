a2b0c82931e12f6bf96f653ee16d9d56938fa95b
hoshikitsunoda <hoshkitsunoda@gmail.com>
2021-03-10 14:50:18 -0800

    close modal on successful submit

--- src/components/AddRecord/AddRecordForm.jsx
+++ src/components/AddRecord/AddRecordForm.jsx
@@ -1,5 +1,12 @@
-import { useState } from 'react'
-import { makeStyles, TextField, Box, Button, Grid } from '@material-ui/core'
+import { useState, useEffect } from 'react'
+import {
+  makeStyles,
+  TextField,
+  Box,
+  Button,
+  Grid,
+  CircularProgress,
+} from '@material-ui/core'
 import { v4 as uuidv4 } from 'uuid'
 
 import { usePostData } from '../../hooks'
@@ -25,7 +32,7 @@ const useStyles = makeStyles((theme) => ({
 const AddRecordForm = ({ handleClose }) => {
   const classes = useStyles()
   const [recordInfo, setRecordInfo] = useState({})
-  const { submitting, postData } = usePostData()
+  const { submitting, close, postData, error } = usePostData()
   const id = uuidv4()
 
   const handleChange = ({ target: { name, value } }) => {
@@ -41,110 +48,142 @@ const AddRecordForm = ({ handleClose }) => {
       id: id,
     }
     postData(formData, id)
-    handleClose()
   }
+
+  useEffect(() => {
+    // closed state from usePostData hook is a source of truth for modal state
+    if (close) {
+      handleClose()
+    }
+  }, [handleClose, close])
 
   return (
     <Grid container spacing={2} className={classes.root}>
-      <Grid item xs={8}>
-        <Box display="flex" flexDirection="row" justifyContent="space-between">
-          <TextField
-            label="Artist"
-            name="artist"
-            variant="filled"
-            fullWidth
-            onChange={handleChange}
-          />
-          <TextField
-            label="Title"
-            name="title"
-            variant="filled"
-            fullWidth
-            onChange={handleChange}
-          />
-        </Box>
-        <TextField
-          label="Label"
-          name="label"
-          variant="filled"
-          fullWidth
-          onChange={handleChange}
-        />
-        <Box display="flex" flexDirection="row" justifyContent="space-between">
-          <TextField
-            label="Format"
-            name="format"
-            variant="filled"
-            fullWidth
-            onChange={handleChange}
-          />
-          <TextField
-            label="Country"
-            name="country"
-            variant="filled"
-            fullWidth
-            onChange={handleChange}
-          />
-        </Box>
-        <Box display="flex" flexDirection="row" justifyContent="space-between">
-          <TextField
-            label="Release year"
-            name="releaseYear"
-            variant="filled"
-            fullWidth
-            onChange={handleChange}
-          />
-          <TextField
-            label="Genre"
-            name="genre"
-            variant="filled"
-            fullWidth
-            onChange={handleChange}
-          />
-        </Box>
-        <TextField
-          label="Location"
-          name="location"
-          variant="filled"
-          fullWidth
-          onChange={handleChange}
-        />
-        <Box display="flex" flexDirection="row" justifyContent="space-between">
-          <TextField
-            label="Media condition"
-            name="mediaCondition"
-            variant="filled"
-            fullWidth
-            onChange={handleChange}
-          />
-          <TextField
-            label="Cover condition"
-            name="coverCondition"
-            variant="filled"
-            fullWidth
-            onChange={handleChange}
-          />
-        </Box>
-        <TextField
-          label="Note"
-          name="note"
-          variant="filled"
-          multiline
-          rows={4}
-          fullWidth
-          onChange={handleChange}
-        />
-      </Grid>
-      <Grid item xs={4}>
-        <Box className={classes.buttons}>
-          <Button variant="contained" color="primary" onClick={handleSubmit}>
-            Add to collection
-          </Button>
-          <Button variant="outlined" color="primary" onClick={handleClose}>
-            Cancel
-          </Button>
-        </Box>
-      </Grid>
+      {error ? (
+        'Something went wrong:('
+      ) : (
+        <>
+          <Grid item xs={8}>
+            <Box
+              display="flex"
+              flexDirection="row"
+              justifyContent="space-between"
+            >
+              <TextField
+                label="Artist"
+                name="artist"
+                variant="filled"
+                fullWidth
+                onChange={handleChange}
+              />
+              <TextField
+                label="Title"
+                name="title"
+                variant="filled"
+                fullWidth
+                onChange={handleChange}
+              />
+            </Box>
+            <TextField
+              label="Label"
+              name="label"
+              variant="filled"
+              fullWidth
+              onChange={handleChange}
+            />
+            <Box
+              display="flex"
+              flexDirection="row"
+              justifyContent="space-between"
+            >
+              <TextField
+                label="Format"
+                name="format"
+                variant="filled"
+                fullWidth
+                onChange={handleChange}
+              />
+              <TextField
+                label="Country"
+                name="country"
+                variant="filled"
+                fullWidth
+                onChange={handleChange}
+              />
+            </Box>
+            <Box
+              display="flex"
+              flexDirection="row"
+              justifyContent="space-between"
+            >
+              <TextField
+                label="Release year"
+                name="releaseYear"
+                variant="filled"
+                fullWidth
+                onChange={handleChange}
+              />
+              <TextField
+                label="Genre"
+                name="genre"
+                variant="filled"
+                fullWidth
+                onChange={handleChange}
+              />
+            </Box>
+            <TextField
+              label="Location"
+              name="location"
+              variant="filled"
+              fullWidth
+              onChange={handleChange}
+            />
+            <Box
+              display="flex"
+              flexDirection="row"
+              justifyContent="space-between"
+            >
+              <TextField
+                label="Media condition"
+                name="mediaCondition"
+                variant="filled"
+                fullWidth
+                onChange={handleChange}
+              />
+              <TextField
+                label="Cover condition"
+                name="coverCondition"
+                variant="filled"
+                fullWidth
+                onChange={handleChange}
+              />
+            </Box>
+            <TextField
+              label="Note"
+              name="note"
+              variant="filled"
+              multiline
+              rows={4}
+              fullWidth
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <Box className={classes.buttons}>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleSubmit}
+              >
+                {submitting ? <CircularProgress /> : 'Add to collection'}
+              </Button>
+              <Button variant="outlined" color="primary" onClick={handleClose}>
+                Cancel
+              </Button>
+            </Box>
+          </Grid>
+        </>
+      )}
     </Grid>
   )
 }

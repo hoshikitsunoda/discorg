a773ee7d0e6c9d6faf118784ed7eeb9d8d98a821
hoshikitsunoda <hoshkitsunoda@gmail.com>
2021-03-20 00:20:23 -0700

    updated to dropzone

--- src/components/AddRecord/ImageUpload.jsx
+++ src/components/AddRecord/ImageUpload.jsx
@@ -1,29 +1,86 @@
-import { useState } from 'react'
+import { useCallback, useMemo } from 'react'
 import firebase from 'firebase'
-import { Input } from '@material-ui/core'
+import { useDropzone } from 'react-dropzone'
+
+const baseStyle = {
+  padding: '0 20px',
+  borderWidth: 2,
+  borderRadius: 2,
+  borderColor: '#b6b6b6',
+  borderStyle: 'dashed',
+  backgroundColor: '#fafafa',
+  color: '#535353',
+  outline: 'none',
+  transition: 'border .24s ease-in-out',
+  fontSize: '14px',
+}
+
+const activeStyle = {
+  borderColor: '#2196f3',
+}
+
+const acceptStyle = {
+  borderColor: '#00e676',
+}
+
+const rejectStyle = {
+  borderColor: '#ff1744',
+}
 
 const ImageUpload = ({ uid, handleFile, handleUrl }) => {
-  const [imageToUpload, setImageToUpload] = useState('')
-  const handleUpload = async () => {
-    const storageRef = firebase
-      .storage()
-      .ref()
-      .child('images')
-      .child(`${uid}.jpg`)
-    await storageRef.put(imageToUpload)
-    storageRef.getDownloadURL().then((url) => handleUrl(url))
-  }
+  const onDrop = useCallback(
+    async (acceptedFiles) => {
+      handleFile(URL.createObjectURL(acceptedFiles[0]))
+      const storageRef = firebase
+        .storage()
+        .ref()
+        .child('images')
+        .child(`${uid}.jpg`)
+      await storageRef.put(acceptedFiles[0])
+      storageRef.getDownloadURL().then((url) => handleUrl(url))
+    },
+    [handleFile, handleUrl, uid]
+  )
 
-  const handleChange = ({ target: { files } }) => {
-    setImageToUpload(files[0])
-    handleFile(URL.createObjectURL(files[0]))
-  }
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject,
+  } = useDropzone({ onDrop })
+
+  const style = useMemo(
+    () => ({
+      ...baseStyle,
+      ...(isDragActive ? activeStyle : {}),
+      ...(isDragAccept ? acceptStyle : {}),
+      ...(isDragReject ? rejectStyle : {}),
+    }),
+    [isDragActive, isDragReject, isDragAccept]
+  )
 
   return (
-    <>
-      <Input type="file" onChange={handleChange} />
-      <button onClick={handleUpload}>Upload</button>
-    </>
+    <div {...getRootProps({ style })}>
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>
+          Drag 'n' drop some files here, or{' '}
+          <span
+            style={{
+              color: '#2196f3',
+              textDecoration: 'underline',
+              cursor: 'pointer',
+            }}
+          >
+            click
+          </span>{' '}
+          to select files
+        </p>
+      )}
+    </div>
   )
 }
 

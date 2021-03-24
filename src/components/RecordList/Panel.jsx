d3a817694b88e2c26530538bbb1b732e051bab6d
hoshikitsunoda <hoshkitsunoda@gmail.com>
2021-03-23 21:58:46 -0700

    added format icons

--- src/components/RecordList/Panel.jsx
+++ src/components/RecordList/Panel.jsx
@@ -4,9 +4,12 @@ import {
   CardActionArea,
   CardContent,
   Typography,
+  Box,
 } from '@material-ui/core'
 import Skeleton from '@material-ui/lab/Skeleton'
 import { Link } from 'react-router-dom'
+import CassetteIcon from '../../images/icons/cassette.png'
+import RecordIcon from '../../images/icons/record.png'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,11 +20,12 @@ const useStyles = makeStyles((theme) => ({
   },
   media: {
     width: '100%',
-    minHeight: 210,
+    borderRadius: 5,
+    paddingBottom: '100%',
   },
   textRight: {
     textAlign: 'right',
-    marginTop: theme.spacing(4),
+    marginTop: theme.spacing(3),
     '& > p': {
       fontSize: 12,
     },
@@ -36,6 +40,9 @@ const useStyles = makeStyles((theme) => ({
     textDecoration: 'none',
     color: theme.palette.primary.main,
   },
+  cardContent: {
+    padding: 8,
+  },
 }))
 
 const Panel = ({ recordData, uid }) => {
@@ -47,6 +54,7 @@ const Panel = ({ recordData, uid }) => {
     label,
     country,
     imageUrl,
+    format,
     id,
   } = recordData[uid]
 
@@ -54,6 +62,25 @@ const Panel = ({ recordData, uid }) => {
     string?.length > maxLength
       ? string?.substring(0, maxLength) + '...'
       : string
+
+  let icon
+
+  switch (format) {
+    case 'LP':
+      icon = RecordIcon
+      break
+    case '2 x LP':
+      icon = RecordIcon
+      break
+    case '3 x LP':
+      icon = RecordIcon
+      break
+    case 'Cassette':
+      icon = CassetteIcon
+      break
+    default:
+      icon = ''
+  }
 
   return (
     <Card className={classes.root}>
@@ -64,35 +91,51 @@ const Panel = ({ recordData, uid }) => {
         className={classes.panel}
       >
         <CardActionArea>
-          {!!imageUrl ? (
-            <img
-              className={classes.media}
-              src={imageUrl}
-              alt={`${artist} - ${title}`}
-            />
-          ) : (
-            <Skeleton variant="rect" width={345} height={200} />
-          )}
-          <CardContent>
-            <Typography variant="h6" component="h2" className={classes.title}>
-              {shortenString(title, 18)}
-            </Typography>
-            <Typography
-              variant="body1"
-              color="textSecondary"
-              component="p"
-              className={classes.artist}
-            >
-              {shortenString(artist, 25)}
-            </Typography>
-            <div className={classes.textRight}>
-              <Typography variant="body2" component="p">
-                {label}
+          <CardContent className={classes.cardContent}>
+            <Box p={1}>
+              {icon && (
+                <Box width={20} mb={2}>
+                  <img src={icon} alt="" style={{ width: '100%' }} />
+                </Box>
+              )}
+              <Typography variant="h6" component="h2" className={classes.title}>
+                {shortenString(title, 18)}
               </Typography>
-              <Typography variant="body2" component="p">
-                {`${releaseYear} / ${country}`}
+              <Typography
+                variant="body1"
+                color="textSecondary"
+                component="p"
+                className={classes.artist}
+              >
+                {shortenString(artist, 25)}
               </Typography>
-            </div>
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                className={classes.textRight}
+              >
+                <Typography variant="body2" component="p">
+                  {label}
+                </Typography>
+                <Typography variant="body2" component="p">
+                  {`${releaseYear} / ${country}`}
+                </Typography>
+              </Box>
+            </Box>
+            {!!imageUrl ? (
+              <div
+                className={classes.media}
+                title={`${artist} - ${title}`}
+                style={{
+                  backgroundImage: `url(${imageUrl})`,
+                  backgroundSize: 'cover',
+                  backgroundRepeat: 'no-repeat',
+                  backgroundPosition: 'center center',
+                }}
+              />
+            ) : (
+              <Skeleton variant="rect" width={345} height={200} />
+            )}
           </CardContent>
         </CardActionArea>
       </Link>

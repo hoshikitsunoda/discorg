9b45d214942821832390c12842eee4195c4365fa
hoshikitsunoda <hoshkitsunoda@gmail.com>
2021-03-27 15:29:59 -0700

    landing page mobile style

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,12 @@
-import { Grid, Box, Typography, Button, makeStyles } from '@material-ui/core'
+import {
+  Grid,
+  Box,
+  Typography,
+  Button,
+  makeStyles,
+  useMediaQuery,
+  useTheme,
+} from '@material-ui/core'
 import { Link } from 'react-router-dom'
 
 import MainLayout from '../hoc/Layout/MainLayout'
@@ -8,32 +16,74 @@ import Album2 from '../images/albums/charles-mingus.jpeg'
 import Album3 from '../images/albums/erykah-badu.jpg'
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column',
+    },
+  },
+  buttons: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginRight: 180,
+
+    [theme.breakpoints.down('sm')]: {
+      margin: '1rem 0',
+      flexDirection: 'column',
+    },
+  },
   button: {
     margin: theme.spacing(2),
     marginLeft: 0,
     width: '100%',
     fontSize: 14,
     textDecoration: 'none',
+
+    [theme.breakpoints.down('sm')]: {
+      marginLeft: 'auto',
+      margin: '0 0 1rem',
+    },
   },
   image: {
     overflow: 'hidden',
+  },
+  gallery: {
+    order: 1,
+
+    [theme.breakpoints.down('sm')]: {
+      order: 2,
+    },
+  },
+  detail: {
+    order: 2,
+
+    [theme.breakpoints.down('sm')]: {
+      order: 1,
+    },
   },
 }))
 
 const Landing = () => {
   const classes = useStyles()
   const albumImages = [Album1, Album2, Album3]
+  const theme = useTheme()
+  const isSmall = useMediaQuery(theme.breakpoints.down('sm'))
 
   return (
     <MainLayout title="home">
-      <Box
-        display="flex"
-        flexDirection="row"
-        justifyContent="space-between"
-        alignItems="center"
-        py={6}
-      >
-        <Box width={1} height={1} p={4}>
+      <Box className={classes.root} py={6}>
+        <Box
+          width={1}
+          height={1}
+          p={isSmall ? 1 : 4}
+          className={classes.gallery}
+        >
           <Grid container spacing={0}>
             {albumImages.map((image, i) => (
               <Grid key={image} item xs={4} className={classes.image}>
@@ -42,10 +92,14 @@ const Landing = () => {
             ))}
           </Grid>
         </Box>
-        <Box width={1} p={4} className={classes.detail}>
+        <Box width={1} p={isSmall ? 1 : 4} className={classes.detail}>
           <Box display="flex" flexDirection="column">
             <Box mt={2} mb={3}>
-              <img src={Logo} alt="discorg homepage" />
+              <img
+                src={Logo}
+                alt="discorg homepage"
+                style={{ width: '100%' }}
+              />
             </Box>
             <Typography variant="h4" component="h4" style={{ fontWeight: 600 }}>
               Social media for record lovers
@@ -56,13 +110,7 @@ const Landing = () => {
             <Typography variant="subtitle1" component="h4">
               Trade records.
             </Typography>
-            <Box
-              display="flex"
-              flexDirection="row"
-              justifyContent="space-between"
-              mt={2}
-              mr="180px"
-            >
+            <Box className={classes.buttons} mt={2}>
               <Link to="/signin" className={classes.button}>
                 <Button
                   type="submit"

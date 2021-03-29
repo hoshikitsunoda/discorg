65b4715d4a42bca61a84fe0f7079dfe22a8d90bf
hoshikitsunoda <hoshkitsunoda@gmail.com>
2021-03-29 15:47:50 -0700

    updated SignIn to use localStorage and context

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { auth } from '../services/firebase'
+import { useStateValue } from '../context/StateProvider'
+import { actionTypes } from '../context/reducer'
 
 const useAuth = () => {
   const history = useHistory()
-  const [data, setData] = useState({})
+  const [credentials, setCredentials] = useState({})
+  const [state, dispatch] = useStateValue()
+  const [user, setUser] = useState('')
 
-  const handleChange = ({ target: { name, value } }) => {
-    setData({
-      ...data,
+  const handleCredentials = ({ target: { name, value } }) => {
+    setCredentials({
+      ...credentials,
       [name]: value,
     })
   }
@@ -25,7 +29,15 @@ const useAuth = () => {
 
   const signIn = async (email, password) => {
     try {
-      await auth().signInWithEmailAndPassword(email, password)
+      const result = await auth().signInWithEmailAndPassword(email, password)
+      dispatch({
+        type: actionTypes.SET_USER,
+        user: result.user,
+      })
+      await localStorage.setItem(
+        'discorg_user_information',
+        JSON.stringify(result.user)
+      )
       history.push('/dashboard')
     } catch (err) {
       toast.error(err.message)
@@ -35,13 +47,32 @@ const useAuth = () => {
   const signOut = async () => {
     try {
       await auth().signOut()
+      dispatch({
+        type: actionTypes.SET_USER,
+        user: null,
+      })
       history.push('/signin')
     } catch (err) {
       toast.error(err.message)
     }
   }
 
-  return { signUp, signIn, data, handleChange, signOut }
+  useEffect(() => {
+    const user = localStorage.getItem('discorg_user_information')
+    if (user) {
+      setUser(JSON.parse(user))
+    }
+  }, [state])
+
+  return {
+    signUp,
+    signIn,
+    handleCredentials,
+    signOut,
+    state,
+    user,
+    credentials,
+  }
 }
 
 export default useAuth
